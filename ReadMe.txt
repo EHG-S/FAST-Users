680dfef4395da089c3d406c6f6fb549f17ce753a
EHG-S <33911181+EHG-S@users.noreply.github.com>
2018-01-09 21:19:50 +0100

    Initial commit

---
+++ ReadMe.txt
@@ -0,0 +1,33 @@
+README
+
+included:
+All source files (.ps1)
+
+
+************
+Description
+************
+
+This script simply imports users into any active directory into the users OU.
+
+
+
+************
+How to Run?
+************
+
+Simply download and run the power shell script as an Administrator. Make sure you have a valid csv file with users (See example in my repository) from which the script reads from.
+
+
+***************
+##Extra Credit:
+***************
+Profiling
+
+I used Notpad++ to profile my results.
+
+Some aide from previous projects helped as well.
+Script was created in my previous school for an exchange server but I have modified it to run on every DC/AD
+
+
+
---
+++ import_users.ps1
@@ -0,0 +1,35 @@
+﻿###########################################
+#created by Cor
+#Modified and revamped by Edward Harris Gomez
+#copyright Techhemel.be
+###########################################
+
+Write-host "Adding users to Active Directory"  "`n"
+
+Write-host "All copyrights EHG" "`n"
+
+Write-host "Scripts starts now!" "`n"
+
+
+#New-ADOrganizationalUnit -Name "Gebruikers"
+
+$Users = Import-Csv -Path "./usersSecurity-local.csv" 
+ForEach ($User in $Users)
+{
+	 
+    $Name = $user.'Name'          
+    $SAM = $User.'SamAccountName'            
+    $UPN = $User.'UserPrincipalName'            
+    $tel = $User.'telephoneNumber'            
+    $Password = "intec-123"     
+    $department = $user.'department'
+    $uac = $user.'userAccountControl'
+    $objClass = $user.'ObjectClass'
+    
+	New-ADUser -Name "$Name" -SamAccountName $SAM -UserPrincipalName $UPN -department $department -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $true –PasswordNeverExpires $flase            
+    
+   
+}
+
+"n"
+write-host "Script is done"
